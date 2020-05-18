4d297ab5bc2f865f5b2f4b84bd1922e474941f83
sunny-g <sunny.gonna@gmail.com>
2020-05-18 14:02:15 -0500

    claim crates.io name

--- src/core/tuntap.rs
+++ src/core/tuntap.rs
@@ -2,8 +2,9 @@ use crate::core::{error::Error, types::*};
 
 const TUN_IPV6_HEADER_LENGTH: u8 = 40;
 
+// TODO? look at https://github.com/actix/actix/blob/master/examples/chat/src/main.rs
+
 /// Represents a running TUN interface.
-///
 ///
 pub trait TunAdapter {
     fn name(&self) -> &str;
@@ -12,3 +13,9 @@ pub trait TunAdapter {
 
     fn start(&mut self);
 }
+
+///
+/// TODO? is an actor that represents a connection (session?) with a remote peer
+///     handles and forwards (streams?) all recv'd packets to to a listener
+///     handles
+pub trait TunConn {}
